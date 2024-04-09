dc0b9242db1f01c17a26f6dca49c69cc46e73d72
Hamad Safdar <hamad.safdar@emumba.com>
2024-04-09 12:21:05 +0500

    Setup done

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        //disclaimer: as per the documentaion, it will use babel.config.js not .json
-        configFile: true,
-      },
-    }),
-  ],
+  plugins: [react()],
 });
